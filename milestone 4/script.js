5424eb8a1a8275b65e2616694fa3b0b4d7e392fe
hibakhaliq <167900653+hibakhaliq@users.noreply.github.com>
2024-09-15 00:44:53 +0500

    Update script.js

--- milestone 4/script.js
+++ milestone 4/script.js
@@ -1,19 +1,25 @@
 var _a;
 (_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
     event.preventDefault();
+var profilePictureInput = document.getElementById('profilePicture');
+    
     var nameElement = document.getElementById('name');
     var emailElement = document.getElementById('email');
     var phoneElement = document.getElementById('phone');
     var educationElement = document.getElementById('education');
     var experienceElement = document.getElementById('experience');
     var skillsElement = document.getElementById('skills');
-    if (nameElement.value && emailElement.value && phoneElement.value && educationElement.value && experienceElement.value && skillsElement.value) {
+    if (ofilePictureInpu && nameElement.value && emailElement.value && phoneElement.value && educationElement.value && experienceElement.value && skillsElement.value) {
         var name_1 = nameElement.value;
         var email = emailElement.value;
         var phone = phoneElement.value;
         var education = educationElement.value;
         var experience = experienceElement.value;
         var skills = skillsElement.value;
+var profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
+        
+        
         //creating resume output
         var resumeOutput = "\n        <h2>Resume</h2>\n        <p><strong>Name:</strong> <span id = \"edit-name\" class = \"editable\" ".concat(name_1, " </span>  </p>\n        <p><strong>Email:</strong> <span id = \"edit-email\" class = \"editable\" ").concat(email, " </span>  </p>\n        <p><strong>Phone:</strong> <span id = \"edit-phone\" class = \"editable\" ").concat(phone, " </span> </p>\n\n        <h3>Eduction</h3>\n        <p id = \"edit-education\" class = \"editable\" >").concat(education, "</p>\n\n        <h3>Experience</h3>\n        <p  id = \"edit-experience\" class = \"editable\">").concat(experience, "</p>\n\n        <h3>Skills</h3>\n        <p  id = \"edit-skills\" class = \"editable\">").concat(skills, "</p>\n        ");
         var resumeOutputElement = document.getElementById('resumeOutput');
